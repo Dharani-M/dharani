fec94e7db41839da80a3d2a66a851e9ba33bf3cc
Dharani-M <dharani.mcse@ksrce.ac.in>
2016-07-20 16:07:08 +0530

    Create Check.java

---
+++ Check.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class Check
+{
+public static void main(String args[])
+{
+int a;
+System.out.println("Enter a value:");
+Scanner s=new Scanner();
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("The given value is positive");
+}
+else
+{
+System.out.println("The given value is negative");
+}
+}}
