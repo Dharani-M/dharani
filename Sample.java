9c9d290f7b47be9810c0191384763b5897e1dc99
Dharani-M <dharani.mcse@ksrce.ac.in>
2016-07-20 15:59:53 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class Sample
+{
+public static void main(String args[])
+{
+int a=9;
+if(a==0)
+{
+System.out.println("a is zero");
+else if(a>0){
+System.out.println("a is positive");
+}
+else
+{
+System.out.println("a is negative");
+}}
