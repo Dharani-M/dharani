0b49738901ff50e01f2c954b387bec607e06087f
Dharani-M <dharani.mcse@ksrce.ac.in>
2016-07-20 16:14:33 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+class Leap
+{
+int a;
+System.out.println("Enter the year");
+Scanner s=new Scanner(System.in);
+a=nextInt();
+if(a%4==)
+{
+System.out.println("Leap year");
+}
+else
+{
+System.out.println("Non-leap year");
+}}
