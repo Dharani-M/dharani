6640b6a4167ed3aa48e5eac014764996ad9b238d
Dharani-M <dharani.mcse@ksrce.ac.in>
2016-07-22 17:23:12 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class Switch
+{
+public static void main(String args[])
+{
+char ch='Q';
+System.out.println("Enter the choice:");
+switch(ch)
+{
+case 'Q':
+    Exit;
+    }}}
